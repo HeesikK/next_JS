fbc969efabbb30e213993bf8884eb2d54f7b45ab
HeesikK <7jjhkhz@naver.com>
2024-05-15 15:04:57 +0900

    feat: 서버 데이터 읽기

--- src/app/read/[id]/page.js
+++ src/app/read/[id]/page.js
@@ -1,7 +1,15 @@
-const Read = ({ params }) => {
+/*
+  read 페이지는 데이터를 화면에 뿌려주기만 하기 때문에 사용자와 상호작용 x -> 서버 컴포넌트로 작업
+*/
+
+const Read = async ({ params }) => {
+  const res = await fetch(`http://localhost:9999/topics/${params.id}`);
+  const topic = await res.json();
+
   return (
     <>
-      <h2>Read</h2>parameters: {params.id}
+      <h2>{topic.title}</h2>
+      {topic.body}
     </>
   );
 };
